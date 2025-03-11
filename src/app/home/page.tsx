dbc98c44522fe3a930566ff23dc230db7b928a7c
Agustin-lozano-2302 <agus.lozano.2302@gmail.com>
2025-03-11 11:07:59 -0300

    fix-white-text-and-arrow-for-the-dropdown

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -66,7 +66,7 @@ export default function Home() {
   },[])
 
   if (isLoading) {
-    return <div>Cargando...</div> // O tu componente de loading
+    return <div className="text-black">Cargando...</div> // O tu componente de loading
   }
 
   return (
@@ -88,13 +88,13 @@ export default function Home() {
             {showMenu && (
               <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10">
                 <div className="px-4 py-2 border-b">
-                  <p className="text-sm text-gray-700 font-semibold">
+                  <p className="text-sm text-black font-semibold">
                   {localUser.first_name}
                   </p>
                 </div>
                 <button
                   onClick={logout}
-                  className="w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 flex items-center gap-2 font-semibold justify-between"
+                  className="w-full text-left px-4 py-2 text-sm text-black hover:bg-gray-100 flex items-center gap-2 font-semibold justify-between"
                 >
                   Cerrar sesión
                   <Image src={"/icons/logout.png"} alt="logout" width={20} height={20} quality={100} />
